1f4b90026985e24f07cb8918c7d22636bbda8657
TomasJefferson2J <115845805+TomasJefferson2J@users.noreply.github.com>
2022-10-14 18:36:15 -0500

    Add files via upload

---
+++ WinFormsApp5/Form1.Designer.vb
@@ -0,0 +1,144 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Textemail = New System.Windows.Forms.TextBox()
+        Me.Textcontrsaeña = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Buttoningresar = New System.Windows.Forms.Button()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Century", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.Location = New System.Drawing.Point(306, 123)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(100, 33)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Email:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Century", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label2.Location = New System.Drawing.Point(235, 228)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(171, 33)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Contraseña:"
+        '
+        'Textemail
+        '
+        Me.Textemail.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.Textemail.Location = New System.Drawing.Point(427, 123)
+        Me.Textemail.Name = "Textemail"
+        Me.Textemail.Size = New System.Drawing.Size(222, 16)
+        Me.Textemail.TabIndex = 2
+        '
+        'Textcontrsaeña
+        '
+        Me.Textcontrsaeña.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.Textcontrsaeña.Location = New System.Drawing.Point(427, 238)
+        Me.Textcontrsaeña.Name = "Textcontrsaeña"
+        Me.Textcontrsaeña.Size = New System.Drawing.Size(222, 16)
+        Me.Textcontrsaeña.TabIndex = 3
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Century", 24.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label3.Location = New System.Drawing.Point(296, 9)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(131, 39)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "LOGIN"
+        '
+        'Buttoningresar
+        '
+        Me.Buttoningresar.Font = New System.Drawing.Font("Century", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Buttoningresar.Location = New System.Drawing.Point(462, 370)
+        Me.Buttoningresar.Name = "Buttoningresar"
+        Me.Buttoningresar.Size = New System.Drawing.Size(144, 28)
+        Me.Buttoningresar.TabIndex = 5
+        Me.Buttoningresar.Text = "Ingresar"
+        Me.Buttoningresar.UseVisualStyleBackColor = True
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Font = New System.Drawing.Font("Century", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.CheckBox1.Location = New System.Drawing.Point(541, 290)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(112, 27)
+        Me.CheckBox1.TabIndex = 6
+        Me.CheckBox1.Text = "Monstrar"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.Black
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(24, 76)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(189, 269)
+        Me.PictureBox1.TabIndex = 7
+        Me.PictureBox1.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.LightGray
+        Me.ClientSize = New System.Drawing.Size(729, 434)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.Buttoningresar)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Textcontrsaeña)
+        Me.Controls.Add(Me.Textemail)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Textemail As TextBox
+    Friend WithEvents Textcontrsaeña As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Buttoningresar As Button
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents PictureBox1 As PictureBox
+End Class
---
+++ WinFormsApp5/Form1.vb
@@ -0,0 +1,46 @@
+﻿Imports System.ComponentModel.DataAnnotations
+
+Public Class Form1
+    Private Sub Buttoningresar_Click(sender As Object, e As EventArgs) Handles Buttoningresar.Click
+        Dim email As String
+
+        Dim contraseña As String
+
+        email = Textemail.Text
+
+        contraseña = Textcontrsaeña.Text
+        If (email = "dd") And (contraseña = "111") Then
+
+            MsgBox("Bienvenido")
+
+            Close()
+
+
+
+        Else
+            MsgBox("email o contraseña incorrecta")
+
+
+
+
+        End If
+
+
+        Textemail.Text = ""
+
+        Textcontrsaeña.Text = ""
+
+
+
+
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+        If CheckBox1.Checked = True Then
+            Textcontrsaeña.UseSystemPasswordChar = False
+        Else
+            Textcontrsaeña.UseSystemPasswordChar = True
+        End If
+
+    End Sub
+End Class
